f765cb33b2da486d23fcf7e6d2b9109d9af4d1f1
jyf-111 <jyfserendipity@outlook.com>
2023-05-24 01:19:08 +0100

    .

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -178,7 +178,7 @@ return {
             },
         })
 
-        local servers = { 'cmake', 'pyright', 'ruff_lsp', 'jsonls', 'html', 'cssls', 'tsserver', 'eslint', "asm_lsp" }
+        local servers = { 'cmake', 'pylsp', 'jsonls', 'html', 'cssls', 'tsserver', 'eslint', "asm_lsp" }
         for _, lsp in ipairs(servers) do
             require 'lspconfig'[lsp].setup {
                 capabilities = capabilities,
