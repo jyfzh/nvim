bbe1a750cf5207a7fdcda96a2a0d86fd41796588
jyf <jyfendipity@outlook.com>
2024-03-15 15:41:56 +0800

    .

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,12 +2,13 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
     },
     config = function()
         require("neo-tree").setup({
             window = {
+                width = 30,
                 auto_expand_width = false
             },
             follow_current_file = {
