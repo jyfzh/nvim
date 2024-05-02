65be0ef00ee96d979ecdd82dd858ad9afbc6df2f
jyf <jyfendipity@outlook.com>
2024-05-02 11:02:28 +0800

    .

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -194,6 +194,7 @@ return {
 
         -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
         local servers = {
+            'typst_lsp',
             'cmake',
             'gopls',
             'pylsp',
