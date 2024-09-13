718427ae03b9d9fb07819c06484f23401de2468e
jyfzh <jyf.work@outlook.com>
2024-09-13 19:51:58 +0800

    .

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -121,6 +121,7 @@ return {
         })
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
         cmp.setup.cmdline({ "/", "?" }, {
+            mapping = cmp.mapping.preset.cmdline(),
             sources = {
                 { name = "buffer" },
             },
