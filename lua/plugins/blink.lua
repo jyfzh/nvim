1e46dff81af6e27dea272291cc447746e86f3012
jiangyufeng <jiangyufeng@windhilltech.com>
2025-04-28 19:17:44 +0800

    .

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,14 +1,39 @@
 return {
     'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
+    dependencies = "fang2hou/blink-copilot" ,
     event = { "BufReadPost", "BufNewFile" },
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
+        completion = {
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 500,
+                treesitter_highlighting = false,
+            },
+            menu = {
+                border = "single",
+            },
+            ghost_text = {
+                enabled = true
+            },
+        },
         keymap = { preset = 'enter' },
         sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
+            default = { 'copilot', 'lsp', 'path', 'snippets', 'buffer' },
+            providers = {
+                copilot = {
+                    name = "copilot",
+                    module = "blink-copilot",
+                    score_offset = 100,
+                    async = true,
+                    opts = {
+                        max_completions = 1,
+                        max_attempts = 2
+                    }
+                },
+            },
         },
         fuzzy = { implementation = "lua" }
     },
